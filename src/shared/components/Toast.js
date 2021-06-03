cd2d80137527d06b7f2a8a135be63cb3d746dbf6
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-03 23:03:12 +0530

    toast component setup

---
+++ src/shared/components/Toast.js
@@ -0,0 +1,17 @@
+// import React, { useEffect } from "react";
+// import "./Toast.css";
+
+// function Toast() {
+//   useEffect(() => {
+//     setTimeout(() => {
+//       closeToast();
+//     }, 3000);
+//   });
+//   return (
+//     <div className={`toast  toast-open`}>
+//       <p>-- This is toast message ! --</p>
+//     </div>
+//   );
+// }
+
+// export default Toast;
