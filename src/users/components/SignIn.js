07e7f7d5a75fa1948e7617040817e580a6335117
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-04 15:06:37 +0530

    setup signin page

--- src/users/components/SignIn.js
+++ src/users/components/SignIn.js
@@ -1,16 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function SignIn({ toggleForm }) {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [formField, setFormField] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formField;
+  const readyToSubmit = email && password;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setErrorMessage("");
+    setFormField((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (readyToSubmit) {
+      try {
+        const user = {
+          email,
+          password,
+        };
+        const { data, status } = await axios.post(
+          "http://localhost:8080/signin",
+          user
+        );
+        console.log(data, status);
+      } catch (err) {
+        return setErrorMessage(err.response.data.message);
+      }
+      console.log("ros");
+    }
+  };
   return (
     <>
-      <form>
-        {/* onSubmit={} */}
+      <form onSubmit={handleSubmit}>
         <h2>Sign In</h2>
-        <input type="email" name="" placeholder="Email" />
-        <input type="password" name="" placeholder="Password" />
-        <p className={`error`}></p>
-        <button>Login</button>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+        <p className={`error ${errorMessage ? "display" : "hidden"}`}>
+          {errorMessage}
+        </p>
+        <button disabled={readyToSubmit ? false : true}>Login</button>
         <p className="signup">
           Don't have an account ?
           <Link to="#" onClick={() => toggleForm()}>
