311d377ef232363869c48a5cd583fa96a58bdcda
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-05 14:33:47 +0530

    feat: navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Navbar from "./shared/components/Navbar";
 import Authenticate from "./users/pages/Authenticate";
 import { Routes, Route } from "react-router-dom";
 
+function Home() {
+  return <div>roshan</div>;
+}
 function App() {
   return (
     <div className="App">
       <Navbar />
       <main>
         <Routes>
+          <Route exact path="/" element={<Home />} />
           <Route path="/authenticate" element={<Authenticate />} />
         </Routes>
       </main>
--- src/shared/components/Navbar.js
+++ src/shared/components/Navbar.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <section className="navigation">
-      <div className="logo">Socialize</div>
+      <NavLink to="/" className="logo">
+        Socialize
+      </NavLink>
+      <div className="navigation-item">
+        <NavLink end to="/">
+          Home
+        </NavLink>
+        <NavLink to="/profile">Profile</NavLink>
+        <NavLink to="/authenticate">Logout</NavLink>
+      </div>
     </section>
   );
 }
