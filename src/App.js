97f24e82f9a9ff5dbe4899d82743f8f606afe1f4
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-06 10:07:19 +0530

    refactor:  moved routes to diff component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import Navbar from "./shared/components/Navbar";
-import Authenticate from "./users/pages/Authenticate";
-import { Routes, Route } from "react-router-dom";
+import RouteComponent from "./RouteComponent";
 
-function Home() {
-  return <div>roshan</div>;
-}
 function App() {
+  console.log("app rendering");
   return (
     <div className="App">
       <Navbar />
       <main>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/authenticate" element={<Authenticate />} />
-        </Routes>
+        <RouteComponent />
       </main>
     </div>
   );
