d082ac6a759dd1c753e3e9f2d5bb125e5ce74608
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-06 18:35:19 +0530

    feat: authorisation to signed in user

--- src/users/pages/Authenticate.js
+++ src/users/pages/Authenticate.js
@@ -1,50 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SignUp from "../components/SignUp";
 import SignIn from "../components/SignIn";
+import { isAuthenticated } from "../../utils/authrelated";
 import "./Authenticate.css";
+import { Navigate } from "react-router-dom";
 
 function Authenticate() {
   const [addClass, setClass] = useState(true);
+  const [isSignedIn, setSignedIn] = useState(false);
   function toggleForm() {
     setClass((prev) => !prev);
   }
+  useEffect(() => {
+    const token = isAuthenticated();
+    if (!token) {
+      setSignedIn(true);
+    }
+  }, []);
+  if (isAuthenticated()) {
+    return <Navigate to="/" replace />;
+  }
   return (
-    <section className="authenticate">
-      <div className={addClass ? "container" : "container active"}>
-        <div className="user signinBx">
-          <div className="imgBx">
-            <div>
-              <p className="heading">Welcome back</p>
-              <p className="para">
-                {" "}
-                To keep connected with us please login with your personal
-                information
-              </p>
+    <>
+      {isSignedIn && (
+        <section className="authenticate">
+          <div className={addClass ? "container" : "container active"}>
+            <div className="user signinBx">
+              <div className="imgBx">
+                <div>
+                  <p className="heading">Welcome back</p>
+                  <p className="para">
+                    {" "}
+                    To keep connected with us please login with your personal
+                    information
+                  </p>
+                </div>
+              </div>
+              <div className="formBx">
+                {/* login form */}
+                <SignIn toggleForm={toggleForm} />
+              </div>
             </div>
-          </div>
-          <div className="formBx">
-            {/* login form */}
-            <SignIn toggleForm={toggleForm} />
-          </div>
-        </div>
-        {/* ********************************************************************************* */}
+            {/* ********************************************************************************* */}
 
-        <div className="user signupBx">
-          <div className="formBx">
-            {/* signup form */}
-            <SignUp toggleForm={toggleForm} />
-          </div>
-          <div className="imgBx">
-            <div>
-              <p className="heading">Hello friend</p>
-              <p className="para">
-                Enter you personal details and start your journey with us
-              </p>
+            <div className="user signupBx">
+              <div className="formBx">
+                {/* signup form */}
+                <SignUp toggleForm={toggleForm} />
+              </div>
+              <div className="imgBx">
+                <div>
+                  <p className="heading">Hello friend</p>
+                  <p className="para">
+                    Enter you personal details and start your journey with us
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 }
 
