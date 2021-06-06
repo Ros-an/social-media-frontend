87f865908349df8f86d716b557eff9f6516d7248
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-06 10:20:33 +0530

    util: fetching local storage item

---
+++ src/utils/authrelated.js
@@ -0,0 +1,8 @@
+export const isAuthenticated = () => {
+  const user = JSON.parse(localStorage.getItem("jwt"));
+  console.log("token", user);
+  if (user) {
+    return true;
+  }
+  return false;
+};
