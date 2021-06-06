7020acabfcf47d3080e150fde0ba5fcae2077085
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-06 10:18:33 +0530

    feat: nav link toggle on authorisation

--- src/shared/components/Navbar.js
+++ src/shared/components/Navbar.js
@@ -1,20 +1,30 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
+import { useAuthContext } from "../../context-api/auth-context";
 
 function Navbar() {
+  const { authorised } = useAuthContext();
+  let toggle = authorised;
+  if (authorised) {
+    localStorage.setItem("linkAccess", true);
+  } else {
+    toggle = JSON.parse(localStorage.getItem("linkAccess"));
+    console.log("toggle", toggle);
+  }
   return (
     <section className="navigation">
-      <NavLink to="/" className="logo">
+      <NavLink to={toggle ? "/" : "/authenticate"} className="logo">
         Socialize
       </NavLink>
-      <div className="navigation-item">
-        <NavLink end to="/">
-          Home
-        </NavLink>
-        <NavLink to="/profile">Profile</NavLink>
-        <NavLink to="/authenticate">Logout</NavLink>
-      </div>
+      {toggle && (
+        <div className="navigation-item">
+          <NavLink end to="/">
+            Home
+          </NavLink>
+          <NavLink to="/profile">Profile</NavLink>
+          <NavLink to="/authenticate">Logout</NavLink>
+        </div>
+      )}
     </section>
   );
 }
