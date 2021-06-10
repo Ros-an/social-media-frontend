3edbbc729267b5291daeabde78654d5e90f23baa
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-10 16:05:03 +0530

    setup: all users component

--- src/users/page/Users.js
+++ src/users/page/Users.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { isAuthenticated } from "../../utils/authrelated";
 import { Navigate } from "react-router-dom";
-
+import { ContentLoader } from "../../shared/components/Loader";
+import UserCard from "../components/UserCard";
+import { getAllUsers } from "../index";
+import "./Users.css";
 function Users() {
+  const [users, setUsers] = useState("");
+
+  useEffect(() => {
+    getAllUsers(setUsers);
+    console.log("users useEffec");
+  }, []);
   if (!isAuthenticated()) {
     return <Navigate to="/authenticate" />;
   }
+
   return (
     <div className="container">
-      <h1>This is user's page</h1>
+      {users ? (
+        <div className="users">
+          {users.map((user) => (
+            <UserCard key={user._id} data={user} />
+          ))}
+        </div>
+      ) : (
+        <ContentLoader />
+      )}
     </div>
   );
 }
