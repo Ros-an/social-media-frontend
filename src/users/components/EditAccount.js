386d02f84b1e82042b036cae1d98d50809840164
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-10 16:01:56 +0530

    setup: edit component

---
+++ src/users/components/EditAccount.js
@@ -0,0 +1,12 @@
+import React from "react";
+import EditIcon from "@material-ui/icons/Edit";
+function EditAccount() {
+  return (
+    <div title="Edit Profile">
+      <EditIcon />
+      <small>Edit</small>
+    </div>
+  );
+}
+
+export default EditAccount;
