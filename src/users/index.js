a90c8dbf27a7e3d5370e122c3e2e3cafbc1b6a8c
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-10 16:03:40 +0530

    feat: delete account function

--- src/users/index.js
+++ src/users/index.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-export const getUserData = async (data) => {
-  const { userId, setUserData, userInfo } = data;
+export const getUserData = async ({ userId, setUserData, userInfo }) => {
   try {
     const { data, status } = await axios.get(
       `${process.env.REACT_APP_API_URL}/user/${userId}`,
@@ -15,5 +14,43 @@ export const getUserData = async (data) => {
     }
   } catch (err) {
     console.log(err.response);
+  }
+};
+
+export const deleteUserAccount = async ({
+  logoutUser,
+  dispatch,
+  userId,
+  userInfo,
+}) => {
+  try {
+    const { data, status } = await axios.delete(
+      `${process.env.REACT_APP_API_URL}/user/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${userInfo().token}`,
+        },
+      }
+    );
+    if (data.success && status === 200) {
+      console.log("data after delete", data);
+      logoutUser(dispatch);
+    }
+  } catch (err) {
+    console.log(err.response);
+  }
+};
+
+export const getAllUsers = async (setUsers) => {
+  try {
+    const { data, status } = await axios.get(
+      `${process.env.REACT_APP_API_URL}/users`
+    );
+    if (data.success && status === 200) {
+      setUsers(data.users);
+      console.log(data);
+    }
+  } catch (err) {
+    console.log("Error:", err.response);
   }
 };
