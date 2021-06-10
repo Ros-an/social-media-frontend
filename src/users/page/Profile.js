def342ed76dadac81a75f055876e202ec80bd9bb
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-10 16:04:32 +0530

    refactor: profile page and added delete feat

--- src/users/page/Profile.js
+++ src/users/page/Profile.js
@@ -3,32 +3,54 @@ import { ContentLoader } from "../../shared/components/Loader";
 import { isAuthenticated, userInfo } from "../../utils/authrelated";
 import { Navigate, useParams } from "react-router-dom";
 import { getUserData } from "../index";
+import ProfileImage from "../../assets/avatar.jpg";
+import BackgroundImage from "../../assets/background.jpg";
+import DeleteAccount from "../components/DeleteAccount";
+import EditAccount from "../components/EditAccount";
+
+import "./Profile.css";
+
 function Profile() {
   const { userId } = useParams();
   const [userData, setUserData] = useState("");
 
   useEffect(() => {
-    if (!userData) {
-      getUserData({ userId, setUserData, userInfo });
-    }
-  }, [userData, userId]);
+    getUserData({ userId, setUserData, userInfo });
+    console.log("profile useEffect");
+  }, [userId]);
 
   if (!isAuthenticated()) {
     return <Navigate to="/authenticate" />;
   }
 
   return (
-    <div className="container">
+    <>
       {userData ? (
-        <>
-          <h3>Name: {userData.name}</h3>
-          <h4>Email: {userData.email}</h4>
-          <h5>Joined {`${new Date(userData.createdAt).toDateString()}`}</h5>
-        </>
+        <section className="profile-card">
+          <div className="profile-background">
+            <img src={BackgroundImage} alt="backgroundImage" />
+          </div>
+          <img src={ProfileImage} alt="profileImg" className="profile-img" />
+          <div className="profile-detail">
+            <p className="name">{userData.name}</p>
+            <p className="headline">Here will be the headline</p>
+            <p className="email">{userData.email}</p>
+            <p className="joined">
+              <span>Joined:</span>{" "}
+              {`${new Date(userData.createdAt).toDateString()}`}
+            </p>
+            {isAuthenticated() && userInfo().user._id === userData._id && (
+              <div className="profile-control">
+                <EditAccount />
+                <DeleteAccount userId={userData._id} />
+              </div>
+            )}
+          </div>
+        </section>
       ) : (
         <ContentLoader />
       )}
-    </div>
+    </>
   );
 }
 
