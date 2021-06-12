4b3694400785aec4542e3173809e86862500fa08
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-12 16:04:22 +0530

    feat: api call related to edit form

--- src/users/index.js
+++ src/users/index.js
@@ -54,3 +54,56 @@ export const getAllUsers = async (setUsers) => {
     console.log("Error:", err.response);
   }
 };
+
+export const userDataForEdit = async ({ userId, setData, userInfo }) => {
+  try {
+    const { data, status } = await axios.get(
+      `${process.env.REACT_APP_API_URL}/user/${userId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${userInfo().token}`,
+        },
+      }
+    );
+    if (data.success && status === 200) {
+      setData((prevValue) => ({
+        ...prevValue,
+        _id: data.user._id,
+        name: data.user.name,
+        email: data.user.email,
+      }));
+    }
+  } catch (err) {
+    console.log(err.response);
+  }
+};
+
+export const updateProfile = async ({
+  userInfo,
+  userId,
+  user,
+  setNavigation,
+  setErrorMessage,
+  setLoading,
+}) => {
+  try {
+    const { data, status } = await axios.post(
+      `${process.env.REACT_APP_API_URL}/user/${userId}`,
+      user,
+      {
+        headers: {
+          Authorization: `Bearer ${userInfo().token}`,
+        },
+      }
+    );
+    console.log("updated data", data, status);
+    if (data.success && status === 200) {
+      setNavigation(true);
+    }
+  } catch (err) {
+    console.log(err.response);
+    setErrorMessage(err.response.data.message);
+  } finally {
+    setLoading(false);
+  }
+};
--- src/users/page/EditProfileForm.js
+++ src/users/page/EditProfileForm.js
@@ -1,36 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { userInfo } from "../../utils/authrelated";
+import { useParams } from "react-router-dom";
+import { userDataForEdit, updateProfile } from "../index";
+import { Navigate } from "react-router-dom";
+import { Loader } from "../../shared/components/Loader";
 
+import "./EditProfileForm.css";
 function EditProfileForm() {
+  const { userId } = useParams();
+  const [errorMessage, setErrorMessage] = useState("");
+  const [navigation, setNavigation] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [data, setData] = useState({
+    _id: "",
+    name: "",
+    email: "",
+    password: "",
+  });
+  const { name, email, password } = data;
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+    setErrorMessage("");
+    setData((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setLoading(true);
+    if (name.length < 3) {
+      return setErrorMessage("username length atleast 3");
+    }
+    if (password.length > 0) {
+      if (password.length < 8) {
+        return setErrorMessage("password must be of atleast 8 characters");
+      }
+    }
+    console.log(data);
+    if (name && email) {
+      let user;
+      if (!password) {
+        user = { name, email };
+      } else {
+        user = { name, email, password };
+      }
+      updateProfile({
+        userInfo,
+        userId,
+        user,
+        setNavigation,
+        setErrorMessage,
+        setLoading,
+      });
+    }
+  };
+  useEffect(() => {
+    userDataForEdit({ setData, userId, userInfo });
+  }, [userId]);
+  if (navigation) {
+    return <Navigate to={`/${userInfo().user.name}/${userId}`} />;
+  }
   return (
-    <>
-      <h1>Edit Profile Form</h1>
-      <form>
+    <div className="container edit-form">
+      <h2>Edit Profile Form</h2>
+      <form onSubmit={handleSubmit}>
         <p>Name</p>
-        <input type="text" placeholder="FullName" />
+        <input
+          type="text"
+          name="name"
+          placeholder="FullName"
+          value={data.name}
+          onChange={handleOnChange}
+        />
         <p>Email</p>
-        <input type="text" placeholder="Email Address" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email Address"
+          value={data.email}
+          onChange={handleOnChange}
+        />
+        <p>Password</p>
+        <input
+          type="password"
+          name="password"
+          placeholder="update password"
+          value={data.password}
+          onChange={handleOnChange}
+        />
+        <p className="error">{errorMessage}</p>
+        <button className="update">{isLoading ? <Loader /> : "UPDATE"}</button>
       </form>
-    </>
+    </div>
   );
 }
 
 export default EditProfileForm;
-
-/* <input type="password" placeholder="Password" />
-<input type="text" placeholder="Website" />
-<input type="text" placeholder="First Name" />
-<input type="text" placeholder="Last Name" /> */
-
-/* <textarea placeholder="Bio" /></textarea> */
-
-/* <select>
-  <option value="">Male</option>
-  <option value="">Female</option>
-</select> */
-
-/* <input type="text" placeholder="Facebook Profile URL" />
-<input type="text" placeholder="Twitter Profile URL" />
-<input type="text" placeholder="Google+ Profile URL" />
-<input type="text" placeholder="LinkedIn Profile URL" /> */
-
-/* <input type="submit" value="Register" /> */
