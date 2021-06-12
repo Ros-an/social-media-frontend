30b5003ad48f53fd77c4a3fda7501e5334222997
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-12 16:00:01 +0530

    refactor: logout button

--- src/auth/components/Logout.js
+++ src/auth/components/Logout.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { useAuthContext } from "../../context-api/auth-context";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { logoutUser } from "../index";
 function Logout() {
   const { dispatch } = useAuthContext();
   return (
-    <div
-      className="navigation-item--link pointer-cursor"
-      onClick={() => logoutUser(dispatch)}
-    >
-      <ExitToAppIcon />
-      <p>Logout</p>
+    <div className="pointer-cursor" onClick={() => logoutUser(dispatch)}>
+      <p className="navigation-item--logout">Logout</p>
     </div>
   );
 }
