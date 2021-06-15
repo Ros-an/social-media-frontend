d937f106a127f8948f3801cef7aef15dc5c275f1
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-15 18:44:17 +0530

    refactor: separate component for user profile image

---
+++ src/users/components/Image.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useRef } from "react";
+
+function Image({ url, photo, id, hasImage, styling }) {
+  const imageRef = useRef();
+  useEffect(() => {
+    console.log("inside image useEffevt");
+    imageRef.current.src = hasImage
+      ? `${process.env.REACT_APP_API_URL}/user/${url}/${id}`
+      : `${photo}`;
+  }, [hasImage, url, id, photo]);
+  return <img ref={imageRef} src={photo} alt={url} className={styling} />;
+}
+
+export default Image;
