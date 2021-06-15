37c4ef1b422070523e055ca6dd03e5111e35f724
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-15 18:45:13 +0530

    feat: show user image if available else default image

--- src/users/components/UserCard.js
+++ src/users/components/UserCard.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./UserCard.css";
 import DefaultAvatar from "../../assets/avatar.jpg";
 import { Link } from "react-router-dom";
 
 function UserCard({ data }) {
+  const imageRef = useRef();
+  useEffect(() => {
+    if (data.userphoto) {
+      imageRef.current.src = `${process.env.REACT_APP_API_URL}/user/photo/${data._id}`;
+    } else {
+      imageRef.current.src = `${DefaultAvatar}`;
+    }
+  });
   return (
     <div className="user-card">
       <Link
         to={`/${data.name.split(" ")[0]}/${data._id}`}
         className="user-card__info"
       >
-        <img src={DefaultAvatar} alt="user" />
+        <div className="image">
+          <img src={DefaultAvatar} ref={imageRef} alt="user" />
+        </div>
         <div className="user-detail">
           <p className="user-name">{data.name}</p>
           <p>
