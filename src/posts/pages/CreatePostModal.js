7c93bf416e06c947f82d8c9a4d379e6ef02f60ae
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-19 07:54:54 +0530

    style: modal

--- src/posts/pages/CreatePostModal.js
+++ src/posts/pages/CreatePostModal.js
@@ -1,67 +1,130 @@
-import React from "react";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
+import React, { useState, useRef } from "react";
 import CloseIcon from "@material-ui/icons/Close";
 import DefaultAvatar from "../../assets/avatar.jpg";
 import ImageIcon from "@material-ui/icons/Image";
+import { userInfo } from "../../utils/authrelated";
 import "./CreatePostModal.css";
 
 export default function TransitionsModal() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
+  const [modalToggle, setModalToggle] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const fileRef = useRef();
+  const formData = new FormData();
+  const [data, setData] = useState({
+    post: "",
+  });
+  const handleChange = (event) => {
+    errorMessage && setErrorMessage("");
+    const { name, files } = event.target;
+    const value = name === "postphoto" ? files[0] : event.target.value;
+    setData((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
   };
-
-  const handleClose = () => {
-    setOpen(false);
+  const uploadFile = () => {
+    fileRef.current.click();
+  };
+  const handleSubmit = () => {
+    console.log("submit button");
+    const { post, postphoto } = data;
+    formData.set("post", post);
+    if (postphoto) {
+      if (postphoto.size < 500000) {
+        formData.set("postphoto", postphoto);
+      } else {
+        console.log("this is error segment");
+        return setErrorMessage("image size < 500KB");
+      }
+    }
+    for (let [name, value] of formData) {
+      console.log(name, value);
+    }
   };
 
   return (
     <div>
       <div className="transition-modal-button">
-        <img src={DefaultAvatar} alt={"user"} className="avatar-img" />
-        <button onClick={handleOpen} className="write-a-post">
+        <img
+          src={`${process.env.REACT_APP_API_URL}/user/photo/${
+            userInfo().user._id
+          }`}
+          alt={"user"}
+          className="avatar-img"
+          onError={(i) => (i.target.src = `${DefaultAvatar}`)}
+        />
+        <button
+          onClick={() => setModalToggle(!modalToggle)}
+          className="write-a-post"
+        >
           write a post.{" "}
         </button>
       </div>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className="modal-position"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
+      {modalToggle && (
+        <div className="modal-position">
           <div className="transition-modal">
             <div className="modal-header">
               <h2 id="transition-modal-title">Create a post</h2>
-              <CloseIcon onClick={handleClose} className="pointer-cursor" />
+              <CloseIcon
+                onClick={() => setModalToggle(!modalToggle)}
+                className="pointer-cursor"
+              />
             </div>
             <div id="transition-modal-description">
               <div className="transition-modal-user">
-                <img src={DefaultAvatar} alt={"user"} className="avatar-img" />
+                <img
+                  src={`${process.env.REACT_APP_API_URL}/user/photo/${
+                    userInfo().user._id
+                  }`}
+                  alt={"user"}
+                  className="avatar-img"
+                  onError={(i) => (i.target.src = `${DefaultAvatar}`)}
+                />
                 <h3>User name will be here</h3>
               </div>
               <textarea
                 type="text"
-                name="body"
+                name="post"
+                value={data.post}
+                onChange={handleChange}
                 placeholder="What do you want to talk about ?"
               />
             </div>
             <div className="transition-modal-submit">
-              <ImageIcon className="pointer-cursor" />
-              <button className="post-btn pointer-cursor">Post</button>
+              <input
+                type="file"
+                name="postphoto"
+                style={{ display: "none" }}
+                accept="images/*"
+                ref={fileRef}
+                onChange={handleChange}
+              />
+              <div className="image-upload">
+                <ImageIcon
+                  className="pointer-cursor image-upload-btn"
+                  onClick={uploadFile}
+                />
+                <small>
+                  <span style={{ color: "red" }}>
+                    {errorMessage && `${errorMessage}`}
+                  </span>
+                  {data.postphoto &&
+                    !errorMessage &&
+                    `${data.postphoto.name.substring(0, 20)}...`}
+                </small>
+              </div>
+              <button
+                disabled={data.post.length === 0 && true}
+                type="button"
+                className="post-btn pointer-cursor"
+                onClick={handleSubmit}
+              >
+                Post
+              </button>
             </div>
           </div>
-        </Fade>
-      </Modal>
+        </div>
+      )}
     </div>
   );
 }
