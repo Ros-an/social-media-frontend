f2d2747cd88abbd52e14241916ff0e1222074e27
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-22 07:27:58 +0530

    setup: local strorage update

--- src/users/page/EditProfile.js
+++ src/users/page/EditProfile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { userInfo } from "../../utils/authrelated";
+import { userInfo, updateLocalData } from "../../utils/authrelated";
 import { useParams } from "react-router-dom";
 import { userDataForEdit, updateProfile } from "../index";
 import { Navigate } from "react-router-dom";
@@ -77,6 +77,7 @@ function EditProfile() {
         setNavigation,
         setErrorMessage,
         setLoading,
+        updateLocalData,
       });
     }
   };
