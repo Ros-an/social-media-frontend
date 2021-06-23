ebb3e6265c2f354d07532b28832c56d38bcf5a61
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-23 05:56:16 +0530

    setup: editpost and singlepost component

--- src/Routing.js
+++ src/Routing.js
@@ -3,8 +3,10 @@ import { Routes, Route } from "react-router-dom";
 import Profile from "./users/page/Profile";
 import Users from "./users/page/Users";
 import EditProfile from "./users/page/EditProfile";
+import EditPost from "./posts/pages/EditPost";
 import PrivateRoute from "./private/PrivateRoute";
 import Feed from "./posts/pages/Feed";
+import SinglePost from "./posts/pages/SinglePost";
 function Routing() {
   return (
     <>
@@ -13,6 +15,8 @@ function Routing() {
         <PrivateRoute path="/users" element={<Users />} />
         <PrivateRoute path="/:user/:userId" element={<Profile />} />
         <PrivateRoute path="/:user/edit/:userId" element={<EditProfile />} />
+        <PrivateRoute path="/:user/post/edit/:postId" element={<EditPost />} />
+        <PrivateRoute path="/:user/post/:postId" element={<SinglePost />} />
         <Route path="/authenticate" element={<Authenticate />} />
       </Routes>
     </>
