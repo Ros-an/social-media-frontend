917d5567737f3ed9f7e47404fae68ee7174e8bd5
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-06-24 17:52:50 +0530

    feat: like unlike

--- src/posts/components/PostCard.js
+++ src/posts/components/PostCard.js
@@ -1,29 +1,54 @@
 import React, { useState } from "react";
 import DefaultAvatar from "../../assets/avatar.jpg";
 import Image from "../../users/components/Image";
+import {Link} from "react-router-dom";
+import {userInfo} from "../../utils/authrelated";
+import { addToLikeList, removeFromLikeList } from "../postApi";
 import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ThumbUpAltRoundedIcon from '@material-ui/icons/ThumbUpAltRounded';
 import InsertCommentOutlinedIcon from "@material-ui/icons/InsertCommentOutlined";
+import {usePostContext} from "../../context-api/PostProvider";
 function PostCard({ data }) {
   const [show, setShow] = useState(false);
+  const [update,setUpdate] = useState(data);
+  const { postDispatch } = usePostContext();
   const {
     post,
     _id,
     postphoto,
+    likes,
     createdAt,
-    postedBy: { name, userphoto },
-  } = data;
+    postedBy: { name },
+  } = update;
+  const like = () => {
+    const userId = userInfo().user._id;
+    const postId = _id;
+    addToLikeList({userInfo,
+      postDispatch,
+      postId,
+      userId,
+      setUpdate})
+  }
+  const unlike = () => {
+    const userId = userInfo().user._id;
+    const postId = _id;
+    removeFromLikeList({userInfo,
+      postDispatch,
+      postId,
+      userId,
+      setUpdate})
+  }
   return (
     <div className="post-card">
       <div className="post-user">
         <Image
           url={"photo"}
           photo={DefaultAvatar}
-          id={data.postedBy._id}
-          hasImage={userphoto}
+          id={update.postedBy._id}
           styling="avatar-img"
         />
         <div className="user-detail">
-          <p className="user-name">{name}</p>
+          <Link to={`/${name.split(" ")[0]}/${update.postedBy._id}`} className="user-name">{name}</Link>
           <small style={{ color: "gray", fontSize: "0.7rem" }}>{`${new Date(
             createdAt
           ).toDateString()}`}</small>
@@ -49,8 +74,9 @@ function PostCard({ data }) {
       </div>
       <div className="post-like-comment">
         <small>
-          <ThumbUpAltOutlinedIcon className="like" />
-          <span>Like</span>
+          {!likes.some(e => e === userInfo().user._id) && <ThumbUpAltOutlinedIcon className="unlike pointer-cursor" onClick={like}/>}
+          {likes.some(e => e === userInfo().user._id) && <ThumbUpAltRoundedIcon className="like pointer-cursor" onClick={unlike}/>}
+          <span>{likes.length === 0 ? "Like": `${likes.length}`}</span>
         </small>
         <small>
           <InsertCommentOutlinedIcon className="comment" />
