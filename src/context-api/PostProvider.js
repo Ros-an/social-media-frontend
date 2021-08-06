821e2940e86a73f188f71bcca81b207d4785ec6b
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2021-08-06 07:27:03 +0530

    refactor: getallPost request from local to global

--- src/context-api/PostProvider.js
+++ src/context-api/PostProvider.js
@@ -1,4 +1,6 @@
 import { useContext, createContext, useReducer } from "react";
+import React, { useEffect, useCallback } from "react";
+import axios from "axios";
 
 const PostContext = createContext();
 const reducer = (state, action) => {
@@ -46,6 +48,22 @@ const initialState = {
 };
 export const PostProvider = ({ children }) => {
   const [state, postDispatch] = useReducer(reducer, initialState);
+  const getAllPosts = useCallback(async () => {
+    try {
+      const { data, status } = await axios.get(
+        `${process.env.REACT_APP_API_URL}/posts`
+      );
+      if (data.success && status === 200) {
+        const posts = data.posts.reverse()
+        postDispatch({ type: "LOAD_POST_DATA", payload: posts });
+      }
+    } catch (err) {
+      console.log(err.response);
+    }
+  }, [postDispatch]);
+  useEffect(() => {
+    getAllPosts();
+  }, [getAllPosts]);
   return (
     <PostContext.Provider value={{ ...state, postDispatch }}>
       {children}
--- src/posts/components/Posts.js
+++ src/posts/components/Posts.js
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useEffect, useCallback } from "react";
+import React from "react";
 import { usePostContext } from "../../context-api/PostProvider";
 import PostCard from "./PostCard";
 import { ContentLoader } from "../../shared/components/Loader";
@@ -7,27 +6,11 @@ import { ContentLoader } from "../../shared/components/Loader";
 import "./Posts.css";
 
 function Posts() {
-  const { posts, postDispatch } = usePostContext();
-  const getAllPosts = useCallback(async () => {
-    try {
-      const { data, status } = await axios.get(
-        `${process.env.REACT_APP_API_URL}/posts`
-      );
-      if (data.success && status === 200) {
-        const posts = data.posts.reverse()
-        postDispatch({ type: "LOAD_POST_DATA", payload: posts });
-      }
-    } catch (err) {
-      console.log(err.response);
-    }
-  }, [postDispatch]);
-  useEffect(() => {
-    getAllPosts();
-  }, [getAllPosts]);
+  const { posts } = usePostContext();
+  
   return (
     <>
-      {posts.length === 0 && <ContentLoader />}
-      {posts && posts.map((post) => <PostCard data={post} key={post._id} />)}
+    {posts.length === 0 ? <ContentLoader/> : posts?.map((post) => (<PostCard data={post} key={post._id} />))}
     </>
   );
 }
