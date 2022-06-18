0de38373204993ffa373e319fb8d04d454c3e02b
Roshan Kr. Mahato <mahatoroshan1999@gmail.com>
2022-06-18 21:10:43 +0530

    feat: pagination

--- src/context-api/PostProvider.js
+++ src/context-api/PostProvider.js
@@ -5,11 +5,29 @@ import axios from "axios";
 const PostContext = createContext();
 const reducer = (state, action) => {
   switch (action.type) {
-    case "LOAD_POST_DATA":
+    case "NO_MORE_POSTS": 
       return {
         ...state,
-        posts: action.payload,
+        btnLoader: false,
+        postMessage: "END"
+      }
+    case "UPADE_PAGE_NO": 
+      return {
+        ...state,
+        btnLoader: true,
+        pageNo: state.pageNo + 1
+      }
+    case "LOAD_POST_DATA": {
+      const newPosts = [...state.posts, ...action.payload];
+      const uniquePosts = Array.from(new Set(newPosts.map(a => a._id))).map(id => {
+         return newPosts.find(a => a._id === id)
+      });
+      return {
+        ...state,
+        btnLoader: false,
+        posts: uniquePosts,
       };
+    }
     case "ADD_POST":
       state.posts.splice(0, 0, action.payload);
       return {
@@ -45,25 +63,36 @@ const reducer = (state, action) => {
 const initialState = {
   posts: [],
   load: false,
+  pageNo: 1,
+  btnLoader: false,
+  postMessage: ""
 };
 export const PostProvider = ({ children }) => {
   const [state, postDispatch] = useReducer(reducer, initialState);
   const getAllPosts = useCallback(async () => {
     try {
       const { data, status } = await axios.get(
-        `${process.env.REACT_APP_API_URL}/posts`
+        `${process.env.REACT_APP_API_URL}/posts?page=${state.pageNo}`
       );
       if (data.success && status === 200) {
-        const posts = data.posts.reverse()
-        postDispatch({ type: "LOAD_POST_DATA", payload: posts });
+        const posts = data.posts;
+        console.log("posts", posts);
+        if(posts.length){
+          postDispatch({ type: "LOAD_POST_DATA", payload: posts });
+        }else{
+          postDispatch({
+            type: "NO_MORE_POSTS"
+          })
+        }
       }
     } catch (err) {
       console.log(err.response);
     }
-  }, [postDispatch]);
+  }, [postDispatch, state.pageNo]);
   useEffect(() => {
     getAllPosts();
   }, [getAllPosts]);
+
   return (
     <PostContext.Provider value={{ ...state, postDispatch }}>
       {children}
--- src/posts/components/Posts.js
+++ src/posts/components/Posts.js
@@ -2,15 +2,26 @@ import React from "react";
 import { usePostContext } from "../../context-api/PostProvider";
 import PostCard from "./PostCard";
 import { ContentLoader } from "../../shared/components/Loader";
+import { Loader } from "../../shared/components/Loader";
 
 import "./Posts.css";
 
 function Posts() {
-  const { posts } = usePostContext();
-  
+  const { posts, postDispatch, btnLoader, postMessage} = usePostContext();
+  const handlePageUpdate = () => {
+    postDispatch({
+      type: "UPADE_PAGE_NO"
+    })
+  }
   return (
     <>
-    {posts.length === 0 ? <ContentLoader/> : posts?.map((post) => (<PostCard data={post} key={post._id} />))}
+    {posts.length === 0 ? <ContentLoader/> : <>
+      {posts?.map((post) => (<PostCard data={post} key={post._id} />))}
+      <div className="load-more__section">
+      {!postMessage && (btnLoader ? <Loader /> : <button className="load-more" onClick={handlePageUpdate}>Load more</button>)}
+      {postMessage && <h3>{postMessage}</h3>}
+      </div>
+    </>}
     </>
   );
 }
